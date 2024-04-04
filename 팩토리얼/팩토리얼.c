26690e1db376bc6e853343a21ae08479e55697cc
martin-yeo <sangsooyeo@gmail.com>
2024-04-05 01:26:15 +0900

    팩토리얼 수정2

--- 팩토리얼/팩토리얼.c
+++ 팩토리얼/팩토리얼.c
@@ -18,7 +18,7 @@ int main(void) {
     printf("%d\n", solution(1));
     printf("%d\n", solution(2));
     printf("%d\n", solution(3628800));
-    printf("%d\n", solution(11));
+    printf("%d\n", solution(50));
 
     return 0;
 }
